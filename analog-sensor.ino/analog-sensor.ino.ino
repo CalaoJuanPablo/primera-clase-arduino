f3fdba935187e762c604cb0fe111ee3b670a790d
Juan Pablo Calao Perez <jcalaop@gmail.com>
2018-03-11 17:57:12 -0500

    sensor de luz

---
+++ analog-sensor.ino/analog-sensor.ino.ino
@@ -0,0 +1,24 @@
+  int led = 13;
+  float sensor = A0;
+  
+ void setup() {
+  // put your setup code here, to run once:
+  
+  pinMode(led,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  int value;
+  value = analogRead(sensor);
+  Serial.println(value);
+  int setpoint = 100;
+
+  if (value < setpoint){
+    digitalWrite(led, 0);
+  }else{
+    digitalWrite(led,1);
+  }
+}
